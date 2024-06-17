39168ec62eebd60c3eb159b98f79aad6f32261cd
cr8t <124842229+cr8t@users.noreply.github.com>
2024-06-17 22:46:39 +0000

    tests: add HWDB entry-value test
    Adds a test to extract an entry from the HWDB.
    
    Author: John Whittington <git@jbrengineering.co.uk>

--- tests/hwdb.rs
+++ tests/hwdb.rs
@@ -11,7 +11,11 @@ fn parse_hwdb() -> Result<()> {
     std::env::set_var("UDEV_HWDB_BIN", "./hwdb.bin");
     let udev = Arc::new(Udev::new());
 
-    let _hwdb = UdevHwdb::new(udev)?;
+    let mut hwdb = UdevHwdb::new(udev)?;
+
+    let entry = hwdb.get_properties_list_entry("usb:v1D6Bp0001", 0).unwrap();
+
+    assert_eq!(entry.value(), "Linux Foundation");
 
     Ok(())
 }
